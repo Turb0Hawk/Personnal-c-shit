5a1393b541f624122302746ec76b5c7f018b220e
David Ringuet School <shelbydav6@gmail.com>
2018-11-16 14:53:39 -0500

    aa

--- L5RDiceRoller/L5RDiceRoller/DiceRollerUI.Designer.cs
+++ L5RDiceRoller/L5RDiceRoller/DiceRollerUI.Designer.cs
@@ -28,13 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRollerUI));
             this.txtResultat = new System.Windows.Forms.RichTextBox();
             this.grpControls = new System.Windows.Forms.GroupBox();
-            this.lblD6 = new System.Windows.Forms.Label();
-            this.lblD12 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnRoll = new System.Windows.Forms.Button();
+            this.txtSkill = new System.Windows.Forms.TextBox();
+            this.txtRing = new System.Windows.Forms.TextBox();
+            this.lblD12 = new System.Windows.Forms.Label();
+            this.lblD6 = new System.Windows.Forms.Label();
             this.grpControls.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,8 +50,8 @@
             // grpControls
             // 
             this.grpControls.Controls.Add(this.btnRoll);
-            this.grpControls.Controls.Add(this.textBox2);
-            this.grpControls.Controls.Add(this.textBox1);
+            this.grpControls.Controls.Add(this.txtSkill);
+            this.grpControls.Controls.Add(this.txtRing);
             this.grpControls.Controls.Add(this.lblD12);
             this.grpControls.Controls.Add(this.lblD6);
             this.grpControls.Location = new System.Drawing.Point(363, 43);
@@ -58,38 +59,6 @@
             this.grpControls.Size = new System.Drawing.Size(174, 157);
             this.grpControls.TabIndex = 1;
             this.grpControls.TabStop = false;
-            // 
-            // lblD6
-            // 
-            this.lblD6.AutoSize = true;
-            this.lblD6.Location = new System.Drawing.Point(6, 31);
-            this.lblD6.Name = "lblD6";
-            this.lblD6.Size = new System.Drawing.Size(115, 13);
-            this.lblD6.TabIndex = 0;
-            this.lblD6.Text = "Nombre de dé de ring :";
-            // 
-            // lblD12
-            // 
-            this.lblD12.AutoSize = true;
-            this.lblD12.Location = new System.Drawing.Point(6, 68);
-            this.lblD12.Name = "lblD12";
-            this.lblD12.Size = new System.Drawing.Size(115, 13);
-            this.lblD12.TabIndex = 1;
-            this.lblD12.Text = "Nombre de dé de skill :";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(127, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(39, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(127, 65);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(39, 20);
-            this.textBox2.TabIndex = 3;
             // 
             // btnRoll
             // 
@@ -101,6 +70,38 @@
             this.btnRoll.UseVisualStyleBackColor = true;
             this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
             // 
+            // txtSkill
+            // 
+            this.txtSkill.Location = new System.Drawing.Point(127, 65);
+            this.txtSkill.Name = "txtSkill";
+            this.txtSkill.Size = new System.Drawing.Size(39, 20);
+            this.txtSkill.TabIndex = 3;
+            // 
+            // txtRing
+            // 
+            this.txtRing.Location = new System.Drawing.Point(127, 28);
+            this.txtRing.Name = "txtRing";
+            this.txtRing.Size = new System.Drawing.Size(39, 20);
+            this.txtRing.TabIndex = 2;
+            // 
+            // lblD12
+            // 
+            this.lblD12.AutoSize = true;
+            this.lblD12.Location = new System.Drawing.Point(6, 68);
+            this.lblD12.Name = "lblD12";
+            this.lblD12.Size = new System.Drawing.Size(115, 13);
+            this.lblD12.TabIndex = 1;
+            this.lblD12.Text = "Nombre de dé de skill :";
+            // 
+            // lblD6
+            // 
+            this.lblD6.AutoSize = true;
+            this.lblD6.Location = new System.Drawing.Point(6, 31);
+            this.lblD6.Name = "lblD6";
+            this.lblD6.Size = new System.Drawing.Size(115, 13);
+            this.lblD6.TabIndex = 0;
+            this.lblD6.Text = "Nombre de dé de ring :";
+            // 
             // DiceRollerUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -108,8 +109,9 @@
             this.ClientSize = new System.Drawing.Size(550, 302);
             this.Controls.Add(this.grpControls);
             this.Controls.Add(this.txtResultat);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "DiceRollerUI";
-            this.Text = "Form1";
+            this.Text = "Legends of the Five Rings Dice Roller";
             this.grpControls.ResumeLayout(false);
             this.grpControls.PerformLayout();
             this.ResumeLayout(false);
@@ -121,8 +123,8 @@
         private System.Windows.Forms.RichTextBox txtResultat;
         private System.Windows.Forms.GroupBox grpControls;
         private System.Windows.Forms.Button btnRoll;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtSkill;
+        private System.Windows.Forms.TextBox txtRing;
         private System.Windows.Forms.Label lblD12;
         private System.Windows.Forms.Label lblD6;
     }
--- L5RDiceRoller/L5RDiceRoller/DiceRollerUI.cs
+++ L5RDiceRoller/L5RDiceRoller/DiceRollerUI.cs
@@ -19,7 +19,17 @@ namespace L5RDiceRoller
 
         private void btnRoll_Click(object sender, EventArgs e)
         {
-
+                int.TryParse(txtRing.Text, out int nbDeeRing);
+                int.TryParse(txtSkill.Text, out int nbDeeSkill);
+                txtResultat.Text += ("Pour " + nbDeeRing + " dés de ring et " + nbDeeSkill + " dés de skill:\n");
+                for (int countDeeRing = 0; countDeeRing < nbDeeRing; ++countDeeRing)
+                {
+                    txtResultat.Text += "\n" + new Dee(6).ToString();
+                }
+                for (int countDeeSkill = 0; countDeeSkill < nbDeeSkill; ++countDeeSkill)
+                {
+                    txtResultat.Text += "\n" + new Dee(12).ToString();
+                }
         }
     }
 }
